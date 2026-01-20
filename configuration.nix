5c5388b63ceeb8a39a8895228b44b8a30346fa01
Shaul Khayyat <shaulyoelkhayyat@gmail.com>
2026-01-20 15:41:59 -0500

    Added some packages and shell scripts

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,8 @@
   environment.systemPackages = with pkgs; [
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   git
+  persepolis
+  neovim
   libreoffice-qt-fresh
   emacs-pgtk
   gimp-with-plugins
