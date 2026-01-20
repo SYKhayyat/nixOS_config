f66fecdb945a1643631238348cd638475514a53c
Shaul Khayyat <shaulyoelkhayyat@gmail.com>
2026-01-20 13:22:20 -0500

    Added gimp and gmic

--- configuration.nix
+++ configuration.nix
@@ -101,8 +101,10 @@
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   git
   libreoffice-qt-fresh
-  emacs-pgtk  
-#  wget
+  emacs-pgtk
+  gimp-with-plugins
+  gimpPlugins.gmic
+  #  wget
   ];
   nix.gc = {
 	automatic = true;
