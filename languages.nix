70949450527767af3cbb7597e943efd6db59c36d
SYKhayyat <shaulyoelkhayyat@gmail.com>
2026-01-23 09:35:22 -0500

    Adding Android support

--- languages.nix
+++ languages.nix
@@ -16,7 +16,7 @@
   ];
   # Alternatively, for environment.variables (slightly different mechanism, but often interchangeable for simple use cases)
   # environment.variables = {
-  ANDROID_SDK_ROOT="${androidsdk}/libexec/android-sdk";
+  ANDROID_SDK_ROOT="${pkgs.androidenv.androidsdk}/libexec/android-sdk";
   # };
 
   # You can also use this module to configure specific programs
