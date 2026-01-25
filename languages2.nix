749be21ab0da615cfcd5cacd4d8520f8b543a205
SYKhayyat <shaulyoelkhayyat@gmail.com>
2026-01-24 22:06:31 -0500

    Setup Backups

---
+++ languages2.nix
@@ -0,0 +1,21 @@
+# my-packages.nix
+{ config, lib, pkgs, ... }:
+
+{ 
+  # This adds packages to the system-wide environment
+  environment.systemPackages = with pkgs; [
+    # Add the packages you want to install
+    gcc # C compiler
+    jdk # Java Development Kit
+    maven # Java Helper.
+    gradle # Java Helper.
+    python315 # Python Scripting language.
+    # You can also add custom-defined packages
+    # (pkgs.callPackage ./path/to/your/custom-package.nix {})
+  ];
+
+
+  # You can also use this module to configure specific programs
+  # For example, to enable the OpenSSH service
+  # services.openssh.enable = true;
+}
---
+++ utilities2.nix
@@ -0,0 +1,22 @@
+# my-packages.nix
+{ config, lib, pkgs, ... }:
+{ 
+  # This adds packages to the system-wide environment
+  environment.systemPackages = with pkgs; [
+    # Add the packages you want to install
+    persepolis # A download manager.
+    peazip # Archive file manager.
+    onedriver # OneDrive manager.
+    git # Version Control.
+    wget # Download from url.
+    htop # Task manager.
+    pandoc # Document conversion.
+    # You can also add custom-defined packages
+    # (pkgs.callPackage ./path/to/your/custom-package.nix {})
+  ];
+
+  # You can also use this module to configure specific programs
+  # For example, to enable the OpenSSH service
+  # services.openssh.enable = true;
+}
+
