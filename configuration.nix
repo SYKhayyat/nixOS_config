4a0c3e4899070db7af265350ad799cb185d51dbe
SYKhayyat <shaulyoelkhayyat@gmail.com>
2026-02-17 16:36:49 -0500

    Added Lix

--- configuration.nix
+++ configuration.nix
@@ -106,7 +106,15 @@ packages = with pkgs; [
     ];
   };
   services.onedrive.enable = true;
-  
+  nixpkgs.overlays = [ (final: prev: {
+    inherit (prev.lixPackageSets.stable)
+      nixpkgs-review
+      nix-eval-jobs
+      nix-fast-build
+      colmena;
+  }) ];
+
+  nix.package = pkgs.lixPackageSets.stable.lix;  
   # Enable the Ollama service
   services.ollama = {
     enable = true;
